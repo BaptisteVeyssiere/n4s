dc377ed992bd4add1713d7a9effeaaa5ae2049bb
Baptiste veyssiere <baptiste.veyssiere@epitech.eugit config --global user.email baptiste.veyssiere@epitech.eugit config --global user.name veyssi_bgit commit -am 1>
2016-05-24 17:42:10 +0200

    package

--- include/ns.h
+++ include/ns.h
@@ -5,10 +5,23 @@
 ** Login   <VEYSSI_B@epitech.net>
 **
 ** Started on  Tue May 24 16:34:27 2016 Baptiste veyssiere
-** Last update Tue May 24 16:34:53 2016 Baptiste veyssiere
+** Last update Tue May 24 17:41:40 2016 Baptiste veyssiere
 */
 
 #ifndef NS_H_
 # define NS_H_
+
+typedef struct	s_info_lidar
+{
+  char		check_fail;
+  char		additionnal_info;
+  double	lidar[32];
+}		t_info_lidar;
+
+typedef struct	s_instruction_return
+{
+  char		check_fail;
+  char		additionnal_info;
+}		t_instruction_return;
 
 #endif /* !NS_H_ */
